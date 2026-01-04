9ad6c6117e9792af359417a6ca6e9efa6007db99
Roger Camps Viader <rcamps-v@car10s4.42barcelona.com>
2026-01-04 15:04:02 +0100

    First commit

---
+++ src/libft/ft_strdup.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcamps-v <rcamps-v@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/26 14:38:19 by rcamps-v          #+#    #+#             */
+/*   Updated: 2025/10/28 12:33:43 by rcamps-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*cpy;
+	int		i;
+
+	i = 0;
+	cpy = malloc(sizeof(*s) * ft_strlen(s) + 1);
+	if (!cpy)
+		return (NULL);
+	while (s[i])
+	{
+		cpy[i] = s[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
+/*
+int	main(void)
+{
+	char	*ptr;
+	ptr = ft_strdup("lol");
+	printf("%s", ptr);
+}
+*/
